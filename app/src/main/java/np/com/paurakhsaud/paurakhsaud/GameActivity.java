2e66e692585f5dead22ad3c37fa2ffe54f350926
Paurakh <paurakhsaud2022@gmail.com>
2022-12-29 08:39:00 +0545

    Game Finish

--- app/src/main/java/np/com/paurakhsaud/paurakhsaud/GameActivity.java
+++ app/src/main/java/np/com/paurakhsaud/paurakhsaud/GameActivity.java
@@ -1,4 +1,5 @@
 package np.com.paurakhsaud.paurakhsaud;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,29 +13,28 @@ import java.util.Arrays;
 public class GameActivity extends AppCompatActivity {
     boolean gameActive = true;
     String winnerStr;
-    Button button;
     // Player representation
     // 0 - X
     // 1 - O
     int activePlayer = 0;
-    int[] gameState = {2, 2 , 2, 2, 2, 2, 2, 2, 2};
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
     //    State meanings:
     //    0 - X
     //    1 - O
     //    2 - Null
-    int[][] winPositions = {{0,1,2}, {3,4,5}, {6,7,8},
-            {0,3,6}, {1,4,7}, {2,5,8},
-            {0,4,8}, {2,4,6}};
+    int[][] winPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+            {0, 4, 8}, {2, 4, 6}};
 
 
-    public void playerTap(View view){
+    public void playerTap(View view) {
         ImageView img = (ImageView) view;
         int tappedImage = Integer.parseInt(img.getTag().toString());
-        if(!gameActive){
+        if (!gameActive) {
             gameReset(view);
         }
 
-        if(gameState[tappedImage] == 2) {
+        if (gameState[tappedImage] == 2) {
             gameState[tappedImage] = activePlayer;
             img.setTranslationY(-1000f);
             if (activePlayer == 0) {
@@ -52,24 +52,22 @@ public class GameActivity extends AppCompatActivity {
         }
 
         // Check if any player has won
-        for(int[] winPosition: winPositions){
-            if(gameState[winPosition[0]] == gameState[winPosition[1]] &&
+        for (int[] winPosition : winPositions) {
+            if (gameState[winPosition[0]] == gameState[winPosition[1]] &&
                     gameState[winPosition[1]] == gameState[winPosition[2]] &&
-                    gameState[winPosition[0]]!=2){
+                    gameState[winPosition[0]] != 2) {
                 // Somebody has won! - Find out who!
                 gameActive = false;
-                if(gameState[winPosition[0]] == 0){
+                if (gameState[winPosition[0]] == 0) {
                     winnerStr = "X has won";
-                }
-                else{
+                } else {
                     winnerStr = "O has won";
                 }
                 // Update the status bar for winner announcement
                 TextView status = findViewById(R.id.status);
                 status.setText(winnerStr);
 
-            }
-            else{
+            } else {
                 winnerStr = "Match Draw! No";
             }
         }
@@ -79,15 +77,15 @@ public class GameActivity extends AppCompatActivity {
         gameActive = true;
         activePlayer = 0;
         Arrays.fill(gameState, 2);
-        ((ImageView)findViewById(R.id.imageView0)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView1)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView2)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView3)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView4)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView5)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView6)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView7)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView8)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView0)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView1)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView2)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView3)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView4)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView5)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView6)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView7)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView8)).setImageResource(0);
 
         TextView status = findViewById(R.id.status);
         status.setText(R.string.taptoplay);
